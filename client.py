83d176f3c244387c8da193b2bd46d7c84471cc2e
notcore <arshinbehjat@gmail.com>
2021-03-13 09:13:10 +0000

    System now supports multiple connections

--- client.py
+++ client.py
@@ -1,64 +1,80 @@
 import socket
 import threading
-# import atexit
 from time import sleep
 
 
-# Checks if message is functional
-def check_msg(message):
-    if message == b'\xf1\xf1':  # idle message
-        return False
-    elif message == b'\xf2\xf2':  # disconnect message
-        print('User has disconnected')
-        return False
-    return True
+class Client:
+
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.peers = {}
+
+    def conn_request(self):
+        data = b'\xf3\xf3'
+        while data == b'\xf3\xf3':
+            print("Sending connection request...")
+            self.sock.sendto(b'', ("139.162.210.80", 5005))
+            data = self.sock.recvfrom(1024)[0]
+            sleep(5)
+        self.peers = self.peers_to_dict(data)
+
+    @staticmethod
+    def peers_to_dict(peers_str):
+        new_peers = {}
+        peers_str = peers_str[2:].decode("UTF-8")
+        peers_list = peers_str.split(", ")
+        for addr in peers_list:
+            addr = addr.split(": ")
+            new_peers[addr[0][1:-1]] = int(addr[1])
+        return new_peers
+
+    # Stops the connection from closing because of idling
+    # Works by sending out a 2 byte-sized packet every 10 seconds
+    def idle_packets(self):
+        for ip, port in self.peers.items():
+            self.sock.sendto(b'\xf1\xf1', (ip, port))
+
+    def update_peers(self):
+        self.sock.sendto(b'', ("139.162.210.80", 5005))
+
+    def keep_alive(self):
+        while True:
+            self.update_peers()
+            self.idle_packets()
+            sleep(5)
+
+    # Checks if message is functional
+    def check_msg(self, msg):
+        if msg == b'\xf1\xf1':  # idle message
+            return False
+        elif b'\xf3\xf3' in msg:  # peer update
+            self.peers = self.peers_to_dict(msg)
+            return False
+        return True
+
+    # Subroutine to catch messages from other clients
+    def recv_messages(self):
+        while True:
+            received_msg, ip = self.sock.recvfrom(1024)
+            if self.check_msg(received_msg):
+                tag = '<{0}>'.format(ip[0])
+                print(tag, received_msg.decode('UTF-8'))
+
+    def start_chat(self):
+        print("Connecting to network...")
+
+        recv_thread = threading.Thread(target=self.recv_messages)
+        keep_alive_thread = threading.Thread(target=self.keep_alive)
+        recv_thread.start()
+        keep_alive_thread.start()  # Does hole punching on first pass
+
+        print("Connect to network. You can now chat.")
+        while True:
+            msg = input('')
+            for ip, port in self.peers.items():
+                self.sock.sendto(bytes(msg, 'UTF-8'), (ip, port))
 
 
-# Subroutine to catch messages from other clients
-def recv_messages(sc):
-    while True:
-        received_msg, ip = sc.recvfrom(1024)
-        if check_msg(received_msg):
-            tag = '<{0}>'.format(ip[0])
-            print(tag, received_msg.decode('UTF-8'))
-
-
-# Stops the connection from closing because of idling
-# Works by sending out a 2 byte-sized packet every 10 seconds
-def idle_packets(sc, cinfo):
-    while True:
-        sc.sendto(b'\xf1\xf1', (cinfo[0], cinfo[1]))
-        sleep(10)
-
-
-# Sends a notification stating the user has disconnected
-def disconnect_send(sc, cinfo):
-    print('Exiting')
-    sc.sendto(b'\xf2\xf2', (cinfo[0], cinfo[1]))
-
-
-# Send request to public server
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto(b'', ('139.162.210.80', 5005))
-
-# Decode peer info
-data = sock.recvfrom(1024)[0]
-data = data.decode('UTF-8').split(',')
-conn_info = (data[0], int(data[1]))
-
-# Send hole punching packet
-print('Connecting to: {0} on port {1}'.format(conn_info[0], conn_info[1]))
-sock.sendto(b'', (conn_info[0], conn_info[1]))
-data = sock.recvfrom(1024)[0]
-
-# atexit.register(disconnect_send, sock, conn_info)
-
-recv_thread = threading.Thread(target=recv_messages, args=[sock])
-idle_thread = threading.Thread(target=idle_packets, args=[sock, conn_info])
-recv_thread.start()
-idle_thread.start()
-
-print('Talking with {0}'.format(conn_info[0]))
-while True:
-    msg = input('')
-    sock.sendto(bytes(msg, 'UTF-8'), (conn_info[0], conn_info[1]))
+c = Client()
+c.conn_request()
+c.start_chat()
--- server.py
+++ server.py
@@ -1,17 +1,26 @@
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("", 5005))
 
-connections = []
-while True:
-    while len(connections) < 2:
-        data, addr = sock.recvfrom(1024)
-        print('Connection request from: {0} on port {1}'.format(addr[0], addr[1]))
-        connections.append((addr[0], str(addr[1])))
+class Server:
 
-    a = connections.pop()
-    b = connections.pop()
-    print('Joining {0} and {1}'.format(a[0], b[0]))
-    sock.sendto(bytes(",".join(b), "UTF-8"), (a[0], int(a[1])))
-    sock.sendto(bytes(",".join(a), "UTF-8"), (b[0], int(b[1])))
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(("", 5005))
+        self.peers = {}  # Keeps track of connected peers in format {IP:PORT}
+
+    # Returns a string version of the peers dictionary without curly brackets
+    def peers_to_str(self):
+        return str(self.peers)[1:-1]
+
+    def listen(self):
+        while True:
+            addr = self.sock.recvfrom(1024)[1]
+            print("Connection request from: {0} on port {1}".format(addr[0], addr[1]))
+            if addr[0] in self.peers:
+                del self.peers[addr[0]]
+            self.sock.sendto(b'\xf3\xf3' + bytes(self.peers_to_str(), "UTF-8"), (addr[0], addr[1]))
+            self.peers[addr[0]] = addr[1]
+
+
+s = Server()
+s.listen()
